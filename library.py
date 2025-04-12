ddffd45143122243d97c6b40d1344e4859df82a7
klaraLuskacova <xluskak00@stud.fit.vut.cz>
2025-04-12 16:32:44 +0200

    Add functions for addition, subtraction, multiplication, and division

--- library.py
+++ library.py
@@ -1,3 +1,40 @@
 # IVS - 2. projekt
 # matematická knihovna pro kalkulačku
 
+"""
+@brief scitani
+@param a prvni cislo 
+@param b druhe cislo 
+@return soucet dvou cisel
+"""
+def add(a,b):   
+    return a+b
+
+"""
+@brief odcitani
+@param a prvni cislo 
+@param b druhe cislo 
+@return rozdil dvou cisel
+"""
+def subtract(a,b):
+    return a-b
+
+"""
+@brief nasobeni
+@param a prvni cislo 
+@param b druhe cislo 
+@return nasobek dvou cisel
+"""
+def multiply(a,b):
+    return a*b
+
+"""
+@brief deleni
+@param a prvni cislo 
+@param b druhe cislo 
+@return podil dvou cisel
+"""
+def devide(a,b):
+    if b == 0:
+        raise ValueError("Nelze dělit nulou.")
+    return a / b
