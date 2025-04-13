08d7427ba036e6ab085d60011f8d9e3ed304eec7
klaraLuskacova <xluskak00@stud.fit.vut.cz>
2025-04-13 12:44:29 +0200

    Fix function power
    Add condition to handle exponent less than zero

--- matlib.py
+++ matlib.py
@@ -55,6 +55,8 @@ def div(a,b):
 def power(a,b):
     if a == 0 and b == 0:
         raise ValueError
+    if b < 0:
+        raise ValueError
     if a < 0 and not float(b).is_integer():
         raise ValueError
     return round(a ** b, 13)
